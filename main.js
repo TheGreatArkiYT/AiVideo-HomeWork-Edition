9fc375f2e1b05a76713228f812c4a7ab219de1f1
Arka Bansal <github.arkbansal@gmail.com>
2021-05-05 21:14:13 +0530

    2.0
    test

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-//Just Cuz Why Not
+objects = [];
+status = "";
+var modlText = "Model Has Loaded";
+
+function setup(){
+    canvas = createCanvas(380 , 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380 , 380);
+    video.hide();
+}
+function modelLoaded() {
+    alert(modlText);
+    console.log(modlText);
+    status = true;
+}
+function start() {
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting";
+    object_name = document.getElementById("name").value;
+}
